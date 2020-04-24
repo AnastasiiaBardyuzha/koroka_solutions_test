559f256e8417700e8e08eaf4e8284ea0948e5f14
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-04-24 12:14:48 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c8fc16614597c3d6d37694c8f1d176a8.js"
+  "./precache-manifest.12ef09a7806f105c082043ccd66dd6c8.js"
 );
 
 self.addEventListener('message', (event) => {
